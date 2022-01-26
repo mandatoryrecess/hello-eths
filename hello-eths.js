04d74f2662280daddfdb41ca128b7568b372f87e
rossanne <rossanneclamp@gmail.com>
2022-01-25 20:57:59 -0800

    ethers downloaded

---
+++ hello-eths.js
@@ -0,0 +1,8 @@
+import { ethers } from "ethers";
+
+const { ethers } = require("ethers");
+
+<script type="module">
+    import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+  
+</script>
